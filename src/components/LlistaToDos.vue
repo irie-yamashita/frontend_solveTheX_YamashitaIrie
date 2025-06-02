e987164fffede85e6a62ef1bedec37f35596e4ac
irieyamashita <irie.yamashita.7e8@itb.cat>
2025-06-02 11:32:37 +0200

    Update component LlistaToDos - Eliminació
    He afegit button per eliminar ToDos

--- src/components/LlistaToDos.vue
+++ src/components/LlistaToDos.vue
@@ -15,11 +15,32 @@ const fetchTodos = async () => {
   }
 };
 
-
 // Executa `fetchTodos()` només quan el component es munta
 onMounted(() => {
   fetchTodos();
 });
+
+
+//Funcions
+
+const preguntarEliminar = (id) => {
+    const eliminar = confirm("Estàs segur/a que vols eliminar el ToDo?");
+
+    if(eliminar) {
+        eliminarToDo(id);
+    }
+};
+
+const eliminarToDo = async (id) => {
+    try { 
+        await fetch(`http://localhost:3000/todos/${id}`, { method: 'DELETE' });
+        todos.value = todos.value.filter(todo => todo.id !== id); //Elimino de la llista el ToDo eliminat
+        console.log("Eliminat!", id);
+    } catch (error) {
+        console.error('Error eliminant el TODO:', error);
+    }
+
+}
 </script>
 
 <template>
@@ -33,6 +54,8 @@ onMounted(() => {
         <p v-if="todo.completat == 0">Completat</p>
         <p v-else>Pendent</p>
         <p>{{ todo.data_creacio}}</p>
+        <button @click="preguntarEliminar(todo.id)">Eliminar ToDo</button>
+        <button @click="preguntarEliminar(todo.id)">Modificar ToDo</button>
       </div>
     </ul>
   </div>
